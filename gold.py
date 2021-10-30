a265fda3a4bfec5a1a1b5a68ef2ffc5bb27d64c0
r567tw <r567tw@gmail.com>
2021-10-31 00:50:51 +0800

    add gold service

---
+++ gold.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+url = "https://rate.bot.com.tw/gold/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+result = soup.find_all("td")
+
+price = int(result[-4].getText())
+
+token = os.getenv("TOKEN")
+notify_url = "https://notify-api.line.me/api/notify"
+
+requests.post(
+    notify_url,
+    headers={'Authorization': "Bearer {}".format(token)},
+    data={"message":"\n今日黃金價格: {}元".format(price)}
+)
