23e6a774c20f0f1b3de64890162156680b036957
r567tw <r567tw@gmail.com>
2022-06-19 22:43:58 +0800

    gold notification

--- gold.py
+++ gold.py
@@ -1,6 +1,7 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import os
+from helpers import notify
 
 url = "https://rate.bot.com.tw/gold/"
 response = requests.get(url)
@@ -10,12 +11,16 @@ result = soup.find_all("td")
 
 price = int(result[2].getText().replace("買進",""))
 
-if (price > 1700):
-    token = os.getenv("TOKEN")
-    notify_url = "https://notify-api.line.me/api/notify"
+want_buy_price = int(os.getenv("GOLD_BUY",1600))
+want_sell_price = int(os.getenv("GOLD_SELL",1700))
 
-    requests.post(
-        notify_url,
-        headers={'Authorization': "Bearer {}".format(token)},
-        data={"message":"\n今日黃金價格: {}元,已大於設定價格,可以考慮賣出".format(price)}
-    )
+content = "\n目前黃金價格: {}元".format(price)
+
+if (price > want_sell_price):
+    content += "\n已大於{}元,可以考慮賣出".format(want_sell_price,price)
+
+if (price < want_buy_price):
+    content += "\n已低於{}元,可以考慮買進".format(want_buy_price,price)
+
+print(content)
+notify.send(content)
