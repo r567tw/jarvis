1809d3f3b1bc3e91c65c9b9ccbbe532f4b9cf7d7
r567tw <r567tw@gmail.com>
2023-12-31 03:34:10 +0800

    Remove commits

--- gold.py
+++ gold.py
@@ -1,4 +1,3 @@
-# import os
 import requests
 from bs4 import BeautifulSoup
 from helpers import notify
@@ -9,21 +8,8 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 result = soup.find_all("td")
 
-# print(result[5].getText())
-
 price = int(result[5].getText().replace("回售", ""))
-
-want_buy_price = 1500  # int(os.getenv("GOLD_BUY",1600))
-want_sell_price = 2000  # int(os.getenv("GOLD_SELL",1700))
 
 content = "\n 黃金價格: ${}".format(price)
 
-if price > want_sell_price:
-    content += "\n 已大於{}元,可以考慮賣出".format(want_sell_price)
-
-if price < want_buy_price:
-    content += "\n 已低於{}元,可以考慮買進".format(want_buy_price)
-
-# print(content)
 notify.send(content)
-# telegram.send(content)
