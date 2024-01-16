f930ea74f67365b4699670affd3c27c8d94ef9a6
r567tw <r567tw@gmail.com>
2024-01-16 23:18:35 +0800

    feature: stock

--- stock.py
+++ stock.py
@@ -1,64 +1,63 @@
 import time
 import twstock
-from helpers import telegram
+from helpers import notify
 
-# 先寫死～
+message = ""
 stocks = [
-    # {'number': '00881',  'ideal': 18, 'decision': False},
-    # {'number': '00878',  'ideal': 15, 'decision': True},
-    {'number': '2891',  'ideal': 20, 'decision': True}
+    {'number': '00878', 'cost': 19.36, 'volume': 1000},
+    {'number': '00888', 'cost': 14.24, 'volume': 6000},
+    {'number': '00896', 'cost': 14.63, 'volume': 1000},
+    {'number': '2891', 'cost': 23.13, 'volume': 1000}
 ]
 
-# 更新台灣的Code表
-twstock.__update_codes()
+need_decisions = ['00888']
+
+def get_stock_info(stock_number):
+    try:
+        price = twstock.realtime.get(stock_number)
+        if price['success']:
+            # print(price)
+            name = price['info']['name']
+            realtime_price = float(price['realtime']['latest_trade_price'])
+            # print(realtime_price)
+            return name, realtime_price
+        else:
+            print(f"無法獲取 {stock_number} 的實時數據")
+            return None, None
+    except Exception as e:
+        print(f"錯誤: {e}")
+        return None, None
+
+def analyze_investment(stock, name, realtime_price):
+    profit = round(realtime_price * stock['volume'] - stock['cost'] * stock['volume'], 2)
+    status = "賺錢中" if profit > 0 else "賠錢中"
+    return f"{name} {status} 預估 {profit} 元\n",profit
+
+total_profit = 0
 
 for stock in stocks:
-    stockNumber = stock['number']
+    stock_number = stock['number']
+    name, realtime_price = get_stock_info(stock_number)
 
-    # 分析 twstock 資料
-    data = twstock.Stock(stockNumber)
-    
-    try:
-        decision = twstock.BestFourPoint(data).best_four_point()
-    except:
-        decision = None
-    
-    price = twstock.realtime.get(stockNumber)
-    if price['realtime']['latest_trade_price'] != "-":
-        realtime_price = int(float(price['realtime']['latest_trade_price']))
-    else:
-        realtime_price = int(float(price['realtime']['best_bid_price'][0]))
+    if name and realtime_price:
+        suggestion,profit = analyze_investment(stock, name, realtime_price)
+        total_profit += profit
+        if profit < 0:
+            message += suggestion
 
-    name = price['info']['name']
+        data = twstock.Stock(stock_number)
+        try:
+            if stock_number in [decision for decision in need_decisions]:
+                decision = twstock.BestFourPoint(data).best_four_point()
+                action = "買進" if decision[0] else "賣出"
+                message += f"建議{action} {name}, 原因：{decision[1]}\n"
+        except Exception as e:
+            message += f"{name}資料過少，無法分析四大買賣點。錯誤: {e}\n"
 
-    message = name
+    time.sleep(5)  # 延遲時間
+    # break
 
-    if (decision[0] == True and decision != None):
-        message += "\ntwStock: 建議買入\n原因：{}".format(decision[1])
-    
-    if (decision[0] == False and decision != None):
-        message += "\ntwStock: 建議賣出\n原因：{}".format(decision[1])
-
-    if stock["decision"]:
-        # 個人決策想買
-        if (realtime_price < stock['ideal']):
-            # 低於購買價格，可能不考慮做任何決策
-            message += "\n{} {}<{} 請參考上面四大賣點建議，考慮買進".format(
-                name, realtime_price, stock['ideal'])
-        else:
-            message = "\n{} {}>{} 不做決策({})".format(
-                name, realtime_price, stock['ideal'], "想買")
-    else:
-        # 個人決策想賣
-        if (realtime_price > stock['ideal']):
-            # 低於購買價格，可能不考慮做任何決策
-            message += "\n{} {}>{} 請參考上面四大賣點建議，考慮賣出".format(
-                name, realtime_price, stock['ideal'])
-        else:
-            message = "\n{} {}<={} 不做決策({})".format(
-                name, realtime_price, stock['ideal'], "想賣")
-
-    if message != "":
-        telegram.send(message)
-        # notify.send(message)
-    time.sleep(60)
+print("Finish !")
+message += f"其餘股票損益為正數\n"
+message += f"預估總損益：{total_profit}"
+notify.send(message=message)
